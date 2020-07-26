64dd0ac2de696b300071913ec769c30bf65f04a6
liuzuyang <liuzuyang@DESKTOP-B6PNTM8>
2020-07-26 15:32:30 +0800

    输出tbb的接口版本信息

--- TestUseCase/TestUseCase/main.cpp
+++ TestUseCase/TestUseCase/main.cpp
@@ -1,8 +1,13 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <tbb/tbb_stddef.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+	qDebug() << "TBB_runtime_interface_version: " << 
+		tbb::TBB_runtime_interface_version();
 
     return a.exec();
 }
